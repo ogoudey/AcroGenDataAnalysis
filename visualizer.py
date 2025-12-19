bdfd3e08e342a6371aa5bded0c3fe20875bd6f68
Olin <olin.goog@gmail.com>
2025-12-19 15:47:39 -0500

    finalized

--- visualizer.py
+++ visualizer.py
@@ -27,7 +27,7 @@ def clean_pupil_sizes(pupil_sizes):
 
     return cleaned
 
-def calculate(pupil_sizes, brightnesses, predict_function):
+def calculate(pupil_sizes, brightnesses, predict_function, N=2, window_size=2):
     """
     Calculate the "average power".
 
@@ -40,48 +40,26 @@ def calculate(pupil_sizes, brightnesses, predict_function):
 
     """
     cleaned_pupil_sizes = clean_pupil_sizes(pupil_sizes)
-    input(f"{cleaned_pupil_sizes} (Hold [enter])")
-    input(brightnesses)
     predicted_pupil_sizes = np.array( [predict_function(l) for l in brightnesses] )
-    input(predicted_pupil_sizes)
     difference = cleaned_pupil_sizes - predicted_pupil_sizes
-    input(difference)
-    N = 2
     y_padded = np.pad(difference, (N//2, N-1-N//2), mode='edge')
-    input(y_padded)
     predicted_pupil_size_moving_avg = np.convolve(y_padded, np.ones((N,))/N, mode='valid')
-    input(predicted_pupil_size_moving_avg)
     power = predicted_pupil_size_moving_avg ** 2
-    input(power)
-    window_size = 2
     moving_avg_power = np.convolve(power, np.ones(window_size)/window_size, mode='valid')
-    input(moving_avg_power)
-    return difference, moving_avg_power
+    return predicted_pupil_size_moving_avg, moving_avg_power
 
 
-
-
-def main(csv_path="data/Calibration_log_11251606_001.csv"):
-    # Splits are within data (assumed)
-
+def plot_full_data_for_one_csv():
+    csv_path="data/Calibration_log_11251606_001.csv"
     # --- Calibrate ---
     increments = get_values_from_data(csv_path)[:17]
     predict_function = fit_increments(increments)
     brightnesses = get_values_from_data(csv_path, "screen_brightness")[:17]
 
-    # --- Plot calibration ---
-    plt.figure()
-    plt.plot(increments, label="Pupil Size (Increments)", linewidth=2)
-    plt.plot(brightnesses, label="Brightness", linewidth=2)
-    plt.plot(np.array( [predict_function(l) for l in brightnesses] ), label="Prediction function", linewidth=2)
-    plt.title("Calibration")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
+    csv_path = "data/Pupil_log_Standard_11251608_001.csv"
     # --- Calculate ---
-    pupil_sizes = get_values_from_data(csv_path)[18:]
-    brightnesses = get_values_from_data(csv_path, "screen_brightness")[18:]
+    pupil_sizes = get_values_from_data(csv_path)
+    brightnesses = get_values_from_data(csv_path, "screen_brightness")
     predicted_pupil_sizes = np.array( [predict_function(l) for l in brightnesses] )
     difference, arousal = calculate(pupil_sizes, brightnesses, predict_function)
 
@@ -103,5 +81,46 @@ def main(csv_path="data/Calibration_log_11251606_001.csv"):
     plt.tight_layout()
     plt.show()
 
+def main(csv_path="data/Calibration_log_11251606_001.csv"):
+    # Splits are within data (assumed)
+
+    # --- Calibrate ---
+    increments = get_values_from_data(csv_path)[:17]
+    predict_function = fit_increments(increments)
+    brightnesses = get_values_from_data(csv_path, "screen_brightness")[:17]
+
+    # --- Plot calibration ---
+    plt.figure()
+    plt.plot(increments, label="Pupil Size (Increments)", linewidth=2)
+    plt.plot(brightnesses, label="Brightness", linewidth=2)
+    plt.plot(np.array( [predict_function(l) for l in brightnesses] ), label="Prediction function", linewidth=2)
+    plt.title("Calibration")
+    plt.legend()
+    plt.tight_layout()
+    #plt.show()
+
+    plt.figure()
+    csvs = {"data/Pupil_log_Standard_11251608_001.csv": "Standard", "data/Pupil_log_AI_11251615_001.csv": "AI"}
+    for csv_path, title in csvs.items():    
+        # --- Calculate ---
+        pupil_sizes = get_values_from_data(csv_path)
+        brightnesses = get_values_from_data(csv_path, "screen_brightness")
+        predicted_pupil_sizes = np.array( [predict_function(l) for l in brightnesses] )
+        difference, arousal = calculate(pupil_sizes, brightnesses, predict_function, N=10, window_size=1)
+
+        print("len(increments):", len(increments))
+        print("len(predicted_pupil_sizes):", len(predicted_pupil_sizes))
+        print("len(pupil_sizes):", len(pupil_sizes))
+        print("len(brightnesses):", len(brightnesses))
+        print("len(arousal):", len(arousal))
+
+        # --- Plot calculation ---
+        plt.plot(difference, label=f"Difference {title}", linewidth=2)
+    plt.title("Difference between Actual and Predicted Pupil Size")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
+    plot_full_data_for_one_csv()
     main()
